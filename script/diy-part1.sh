f3a2d7d253838e00673ce1e24e2bc243d087cd27
TwoOnefour <lys214412@gmail.com>
2024-12-05 23:07:44 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+echo "\nsrc-git smartdns https://github.com/pymumu/smartdns.git" >> feeds.conf.default
 # Add feed sources
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
