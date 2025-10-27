34e5187c373488ded76bc42234a7a9098e4aac46
TwoOnefour <lys214412@gmail.com>
2025-10-27 23:08:51 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -10,8 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo "src-git smartdns https://github.com/pymumu/openwrt-smartdns;master" >> feeds.conf.default
 echo "src-git lucismartdns https://github.com/pymumu/luci-app-smartdns.git;master" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall;main" >> feeds.conf.default
