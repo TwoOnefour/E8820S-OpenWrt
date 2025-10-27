db3ea987aaf5f30a8b77618d4d7680223f567549
TwoOnefour <lys214412@gmail.com>
2025-10-27 15:58:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-apt update
-apt install build-essential clang flex bison g++ gawk \
+sudo apt update
+sudo apt install build-essential clang flex bison g++ gawk \
   gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
   python3-setuptools rsync swig unzip zlib1g-dev file wget
 
